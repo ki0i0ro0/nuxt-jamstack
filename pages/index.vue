4864d585f1ab670cb30914fac72f969feab96d0d
matsumoto_ke <matsumoto_ke@micware.co.jp>
2022-01-13 19:06:32 +0900

    [add] base pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,31 @@
 <template>
-  <Tutorial />
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import axios from 'axios'
 
 export default Vue.extend({
   name: 'IndexPage',
+
+  async asyncData() {
+    const { data } = await axios.get(
+      // your-service-id部分は自分のサービスidに置き換えてください
+      'https://ki0i0ro0.microcms.io/api/v1/nuxt-jamstack',
+      {
+        // your-api-key部分は自分のapi-keyに置き換えてください
+        headers: { 'X-MICROCMS-API-KEY': 'e882cb286ca1422c8ebdbd594b43d5fd3515' }
+      }
+    )
+    return data
+  }
 })
 
 </script>
